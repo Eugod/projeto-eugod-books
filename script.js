87497ca9350990105a22796baa2e78ae2e2787fc
Eugênio Rodrigues <eugenio28.04@gmail.com>
2023-04-11 15:01:36 -0300

    usando then, catch e finally

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-let consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/');
-
-console.log(consultaCEP);
+let consultaCEP = fetch('https://viacep.com.br/ws/88139137/json/')
+    .then(resposta => resposta.json())
+    .then(r => {
+        if(r.erro) {
+            throw new Error('CEP inválido!');
+        } else {
+            console.log(r);
+        }    
+    })
+    .catch(erro => {
+        erro.message = 'CEP inválido'
+        console.log(erro)
+    })
+    .finally(() => console.log('Processamento concluído!'));
